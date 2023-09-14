0c7f3add0fb2953eb18e4d29561942b5862281b4
sunnytechie <sunnyict001@gmail.com>
2023-09-14 21:24:21 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">    
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <title>Lemom Gate Info. Tech. Comm.</title>
@@ -16,31 +16,31 @@
         <meta property="og:image:height" content="200" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:alt" content="{{ config('app.name') }}" />
-    
+
         <!-- Favicon -->
         <link href="assets/img/lemon.png" rel="icon">
-    
+
         <!-- Google Web Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500&family=Jost:wght@500;600;700&display=swap" rel="stylesheet"> 
-    
+        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500&family=Jost:wght@500;600;700&display=swap" rel="stylesheet">
+
         <!-- Icon Font Stylesheet -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-    
+
         <!-- Libraries Stylesheet -->
         <link href="assets/lib/animate/animate.min.css" rel="stylesheet">
         <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
         <link href="assets/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-    
+
         <!-- Customized Bootstrap Stylesheet -->
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    
+
         <!-- Template Stylesheet -->
         <link href="assets/css/style.css" rel="stylesheet">
     </head>
-    
+
     <body>
         <div class="container-xxl bg-white p-0">
             <!-- Spinner Start -->
@@ -50,8 +50,8 @@
                 </div>
             </div>
             <!-- Spinner End -->
-    
-    
+
+
             <!-- Navbar & Hero Start -->
             <div class="container-xxl position-relative p-0" id="home">
                 <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
@@ -81,15 +81,15 @@
                         <a href="mailto:email@example.com" class="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Get a Quote</a>
                     </div>
                 </nav>
-    
+
                 <div class="container-xxl bg-primary hero-header">
                     <div class="container px-lg-5">
                         <div class="row g-5 align-items-end">
                             <div class="col-lg-6 text-center text-lg-start">
                                 <h1 class="text-white mb-4 animated slideInDown">A Digital Agency Of Inteligents & Creative People</h1>
                                 <p class="text-white pb-3 animated slideInDown">LEGIT, Creating Business Growth and Innovation
-                                    Because Tech and Digitalization are changing the way we interact with each other. 
-                                    LEGIT knows that Organizations that can adapt, will be successful. So, we are here 
+                                    Because Tech and Digitalization are changing the way we interact with each other.
+                                    LEGIT knows that Organizations that can adapt, will be successful. So, we are here
                                     for you.
                                     </p>
                                 <a href="#project" class="btn btn-secondary py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft">Read More</a>
@@ -103,8 +103,8 @@
                 </div>
             </div>
             <!-- Navbar & Hero End -->
-    
-    
+
+
             <!-- Feature Start -->
             <div class="container-xxl py-5">
                 <div class="container py-5 px-lg-5">
@@ -113,8 +113,8 @@
                             <div class="feature-item bg-light rounded text-center p-4">
                                 <i class="fa fa-3x fa-mail-bulk text-primary mb-4"></i>
                                 <h5 class="mb-3">Digital Marketing</h5>
-                                <p class="m-0">Your brand is the message your business is trying to communicate with your 
-                                    people. <br>It is imperative that you devise a perfect means to spread your business 
+                                <p class="m-0">Your brand is the message your business is trying to communicate with your
+                                    people. <br>It is imperative that you devise a perfect means to spread your business
                                     message.</p>
                             </div>
                         </div>
@@ -122,7 +122,7 @@
                             <div class="feature-item bg-light rounded text-center p-4">
                                 <i class="fa fa-3x fa-search text-primary mb-4"></i>
                                 <h5 class="mb-3">SEO & Backlinks</h5>
-                                <p class="m-0">You don’t wish to run an invisible business; we will make your services appear On 
+                                <p class="m-0">You don’t wish to run an invisible business; we will make your services appear On
                                     Page 1 On Search Engines. Rank Higher On Searches. Get Discovered Online Easily.
                                     </p>
                             </div>
@@ -131,8 +131,8 @@
                             <div class="feature-item bg-light rounded text-center p-4">
                                 <i class="fa fa-3x fa-laptop-code text-primary mb-4"></i>
                                 <h5 class="mb-3">Design & Development</h5>
-                                <p class="m-0">We ensure that all our Web and Mobile applications solution projects are not only 
-                                    engineered using the most cutting-edge technologies, but also, they're created 
+                                <p class="m-0">We ensure that all our Web and Mobile applications solution projects are not only
+                                    engineered using the most cutting-edge technologies, but also, they're created
                                     through a human-centered design process.</p>
                             </div>
                         </div>
@@ -140,8 +140,8 @@
                 </div>
             </div>
             <!-- Feature End -->
-    
-    
+
+
             <!-- About Start -->
             <div class="container-xxl py-5" id="about">
                 <div class="container py-5 px-lg-5">
@@ -149,8 +149,8 @@
                         <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                             <p class="section-title text-secondary">About Us<span></span></p>
                             <h1 class="mb-5">#1 Digital solution with 10 years of experience</h1>
-                            <p class="mb-4">At LEGIT we wish to simplify the difficulties surrounding all areas of ICT by 
-                                building and deploying deep expertise to propel the businesses that shape the 
+                            <p class="mb-4">At LEGIT we wish to simplify the difficulties surrounding all areas of ICT by
+                                building and deploying deep expertise to propel the businesses that shape the
                                 world.
                                 </p>
                             <div class="skill mb-4">
@@ -189,8 +189,8 @@
                 </div>
             </div>
             <!-- About End -->
-    
-    
+
+
             <!-- Facts Start -->
             <div class="container-xxl bg-primary fact py-5 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="container py-5 px-lg-5">
@@ -219,8 +219,8 @@
                 </div>
             </div>
             <!-- Facts End -->
-    
-    
+
+
             <!-- Service Start -->
             <div class="container-xxl py-5" id="service">
                 <div class="container py-5 px-lg-5">
@@ -235,8 +235,8 @@
                                     <i class="fa fa-search fa-2x"></i>
                                 </div>
                                 <h5 class="mb-3">SEO Optimization</h5>
-                                <p class="m-0">You don’t wish to run an invisible business; we will make your services appear On 
-                                    Page 1 On Search Engines. Rank Higher on Searches. Get Discovered Online Easily. 
+                                <p class="m-0">You don’t wish to run an invisible business; we will make your services appear On
+                                    Page 1 On Search Engines. Rank Higher on Searches. Get Discovered Online Easily.
                                     </p>
                                 <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
                             </div>
@@ -247,8 +247,8 @@
                                     <i class="fa fa-laptop-code fa-2x"></i>
                                 </div>
                                 <h5 class="mb-3">Web Design</h5>
-                                <p class="m-0">We ensure that all our web development and application solution projects are not 
-                                    only engineered using the most cutting-edge technologies, but also, they're 
+                                <p class="m-0">We ensure that all our web development and application solution projects are not
+                                    only engineered using the most cutting-edge technologies, but also, they're
                                     created through a human-centered design process.</p>
                                 <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
                             </div>
@@ -259,8 +259,8 @@
                                     <i class="fab fa-facebook-f fa-2x"></i>
                                 </div>
                                 <h5 class="mb-3">Social Media Marketing</h5>
-                                <p class="m-0">Your brand is the message your business is trying to communicate with your 
-                                    people. It is imperative that you devise a perfect means to spread your business 
+                                <p class="m-0">Your brand is the message your business is trying to communicate with your
+                                    people. It is imperative that you devise a perfect means to spread your business
                                     message.</p>
                                 <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
                             </div>
@@ -271,7 +271,7 @@
                                     <i class="fa fa-credit-card fa-2x"></i>
                                 </div>
                                 <h5 class="mb-3">POS Solutions</h5>
-                                <p class="m-0">LEGIT Point of Sale Solution is a product management software built for you, no 
+                                <p class="m-0">LEGIT Point of Sale Solution is a product management software built for you, no
                                     matter what industry your product serves.</p>
                                 <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
                             </div>
@@ -282,7 +282,7 @@
                                     <i class="fab fa-android fa-2x"></i>
                                 </div>
                                 <h5 class="mb-3">App Programming</h5>
-                                <p class="m-0">At LEGIT we customize mobile application development and help clients to 
+                                <p class="m-0">At LEGIT we customize mobile application development and help clients to
                                     conceptualize, plan and roll out their mobility journey.</p>
                                 <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
                             </div>
@@ -293,7 +293,7 @@
                                     <i class="fa fa-phone-square fa-2x"></i>
                                 </div>
                                 <h5 class="mb-3">24/7 Support</h5>
-                                <p class="m-0">Our friendly customer support agents are always online/phone to support you 
+                                <p class="m-0">Our friendly customer support agents are always online/phone to support you
                                     whenever you need them.</p>
                                 <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
                             </div>
@@ -302,8 +302,8 @@
                 </div>
             </div>
             <!-- Service End -->
-    
-    
+
+
             <!-- Newsletter Start -->
             <div class="container-xxl bg-primary newsletter py-5 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="container py-3 px-lg-5">
@@ -321,8 +321,8 @@
                 </div>
             </div>
             <!-- Newsletter End -->
-    
-    
+
+
             <!-- Projects Start -->
             <div class="container-xxl py-5" id="project">
                 <div class="container py-5 px-lg-5">
@@ -443,8 +443,8 @@
                 </div>
             </div>
             <!-- Projects End -->
-    
-    
+
+
             <!-- Testimonial Start -->
             {{-- <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="container py-5 px-lg-5">
@@ -485,8 +485,8 @@
                 </div>
             </div> --}}
             <!-- Testimonial End -->
-    
-    
+
+
             <!-- Team Start -->
             <div class="container-xxl py-5">
                 <div class="container py-5 px-lg-5">
@@ -495,7 +495,7 @@
                         <h1 class="text-center mb-5">Our Team Members</h1>
                     </div>
                     <div class="row g-4">
-                        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                             <div class="team-item bg-light rounded">
                                 <div class="text-center border-bottom p-4">
                                     <img class="img-fluid rounded-circle mb-4" src="assets/img/2.png" alt="">
@@ -510,7 +510,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                             <div class="team-item bg-light rounded">
                                 <div class="text-center border-bottom p-4">
                                     <img class="img-fluid rounded-circle mb-4" src="assets/img/3.png" alt="">
@@ -525,7 +525,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                             <div class="team-item bg-light rounded">
                                 <div class="text-center border-bottom p-4">
                                     <img class="img-fluid rounded-circle mb-4" src="assets/img/1.png" alt="">
@@ -540,12 +540,27 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                            <div class="team-item bg-light rounded">
+                                <div class="text-center border-bottom p-4">
+                                    <img class="img-fluid rounded-circle mb-4" src="assets/img/team4.jpg" alt="">
+                                    <h5>Joseph Chukwuebuka</h5>
+                                    <span>Media and Graphic Lead</span>
+                                </div>
+                                <div class="d-flex justify-content-center p-4">
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <!-- Team End -->
-            
-    
+
+
             <!-- Footer Start -->
             <div class="container-fluid bg-primary text-light footer wow fadeIn" data-wow-delay="0.1s" id="contact">
                 <div class="container py-5 px-lg-5">
@@ -610,11 +625,11 @@
                     <div class="copyright">
                         <div class="row">
                             <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                                &copy; <a class="border-bottom" href="#">LemonGate</a>, All Right Reserved. 
-                                
+                                &copy; <a class="border-bottom" href="#">LemonGate</a>, All Right Reserved.
+
                                 <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                                <br><br>
-                                
+
                             </div>
                             <div class="col-md-6 text-center text-md-end">
                                 Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>  Distributed By a <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
@@ -630,12 +645,12 @@
                 </div>
             </div>
             <!-- Footer End -->
-    
-    
+
+
             <!-- Back to Top -->
             <a href="#" class="btn btn-lg btn-secondary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
         </div>
-    
+
         <!-- JavaScript Libraries -->
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -646,9 +661,9 @@
         <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
         <script src="assets/lib/isotope/isotope.pkgd.min.js"></script>
         <script src="assets/lib/lightbox/js/lightbox.min.js"></script>
-    
+
         <!-- Template Javascript -->
         <script src="assets/js/main.js"></script>
     </body>
-    
+
     </html>
